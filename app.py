02e52fbf3df073cd8fe68fcf8fe80f73382c8c8f
qadadri <q.haytam@gmail.com>
2022-05-08 16:16:55 +0200

    Testing SqlLite3 DB

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import re
 import sqlite3
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://gpvdoosdnhalss:e56b7c6c239077ff4626fc1676574a9e65d3ff91a038f9914a3be3dccab869ad@ec2-52-200-215-149.compute-1.amazonaws.com:5432/d37ghl7sn5b9k9"
+
 
 @app.before_first_request
 def parsing():
@@ -18,7 +18,7 @@ def parsing():
     df = pd.read_html(table_text)
     df = pd.concat(df)
     df = df.drop(df.columns[[0, 1, -1]], axis=1)
-    conn = sqlite3.connect(app.config['SQLALCHEMY_DATABASE_URI']) 
+    conn = sqlite3.connect('bridge_db') 
     df.to_sql('bridges', conn, if_exists='replace', index = False)   
     conn.commit()
     conn.close()
